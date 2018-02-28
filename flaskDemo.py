4d7c07d65f07901337e4760f384c2b8375a9e3df
allen <1043787207@qq.com>
2018-02-28 16:45:10 +0800

    注册,登录,表单简单验证,后台处理,debug调试模式

--- flaskDemo.py
+++ flaskDemo.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import utils
 from orm import User, DBSession
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -60,22 +61,54 @@ def vue_test():
     return render_template('vueTest.html')
 
 
-@app.route('/signin', methods=['GET'])
+@app.route('/sign_in', methods=['GET'])
 def sign_in():
-    return render_template('form.html', message='wb')
+    return render_template('register.html')
 
 
 @app.route('/signin', methods=['POST'])
 def save_user():
-    name = request.form['name']
-    password = request.form['password']
-    user = User(id=utils.get_uuid(), name=name)
+    name = request.form.get('name', default=None)
+    password = request.form.get('password', default=None)
+    email = request.form.get('email', default=None)
+    sex = request.form.get('sex', default=None)
     session = DBSession()
-    session.add(user)
-    session.commit()
-    session.close()
-    return render_template('signin-ok.html', name=name)
+    u = session.query(User).filter(User.name == name).first()
+    if u is not None:
+        session.close()
+        error_message = '用户名' + name + '已存在,请重新输入'
+        return render_template('register.html', message=error_message)
+    else:
+        user = User(id=utils.get_uuid(), name=name, password=password, email=email, sex=sex, create_time=datetime.now())
+        session.add(user)
+        session.commit()
+        session.close()
+        return render_template('login.html', name=name, password=password)
+
+
+@app.route('/to_login')
+def to_login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    name = request.form.get('name', default=None)
+    password = request.form.get('password', default=None)
+    session = DBSession()
+    user = session.query(User).filter(User.name == name, User.password == password).all()
+    if user is not None and len(user) == 1:
+        session.close()
+        message = '用户名' + name + '登录成功'
+        return render_template('index.html', message=message)
+    else:
+        message = '用户名或密码错误'
+        dict = {'name': name, 'age': 12}
+        dict['sex'] = '男'
+        dict['message'] = message
+
+        return render_template('login.html', dict1=dict)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- orm.py
+++ orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, Integer, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,8 +9,33 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'  # 表的结构
-    id = Column(String(20), primary_key=True)
+    id = Column(String(32), primary_key=True)
     name = Column(String(20))
+    password = Column(String(20))
+    sex = Column(Integer)
+    email = Column(String(20))
+    create_time = Column(DateTime)
+
+
+class Blog(Base):
+    __tablename__ = 'blog'
+    id = Column(String(32), primary_key=True)
+    user_id = Column(String(32))
+    user_name = Column(String(20))
+    name = Column(String(20))
+    summary = Column(String(200))
+    content = Column(String(900))
+    create_at = Column(DateTime)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(String(32), primary_key=True)
+    blog_id = Column(String(32))
+    user_id = Column(String(32))
+    user_name = Column(String(20))
+    content = Column(String(900))
+    create_at = Column(DateTime)
 
 
 #  初始化数据库连接:
